f86f65b4ab1d08dd89ef771ff7686ca829d0262c
Thomas <>
2020-07-22 18:30:50 +0200

    Revert "Change to Cupertino app...why not"
    This reverts commit c20b674cc394e4fbd16272d6582e05ef0f21a6ea.

--- lib/components/stories.dart
+++ lib/components/stories.dart
@@ -24,85 +24,46 @@ makeStories() {
       ));
 }
 
-Container storyItem(
-    {bool hasStory, String url, bool isOwner = false, String username}) {
+storyItem({bool hasStory, String url, bool isOwner = false, String username}) {
   return Container(
-    margin: EdgeInsets.only(right: 20),
+    width: 19.4 * SizeConfig.widthMultiplier,
+    margin: EdgeInsets.only(top: 5, right: 15, bottom: 5),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(colors: [
+        Colors.red,
+        Colors.purple,
+      ]),
+      borderRadius: BorderRadius.circular(45),
+    ),
     child: Stack(
+      // overflow: Overflow.visible,
       children: [
         Container(
-          height: 70,
-          width: 19.4 * SizeConfig.widthMultiplier,
-          // margin: EdgeInsets.only(top: 5, right: 15, bottom: 5),
+          margin: hasStory
+              ? EdgeInsets.all(0.83 * SizeConfig.widthMultiplier)
+              : null,
           decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Colors.red,
-              Colors.purple,
-            ]),
             borderRadius: BorderRadius.circular(45),
-          ),
-          child: Container(
-            height: 70,
-            width: 19.4 * SizeConfig.widthMultiplier,
-            margin: hasStory
-                ? EdgeInsets.all(0.83 * SizeConfig.widthMultiplier)
-                : null,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(45),
-              image:
-                  DecorationImage(image: NetworkImage(url), fit: BoxFit.cover),
-            ),
+            image: DecorationImage(image: NetworkImage(url), fit: BoxFit.cover),
           ),
         ),
+        isOwner
+            ? Positioned(
+                bottom: 0,
+                right: 0.56 * SizeConfig.widthMultiplier,
+                child: addStoryIcon(),
+              )
+            : Container(),
         Positioned(
           bottom: 0,
-          child: Text(username),
+          child: Text(
+            username,
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       ],
     ),
   );
-  // return Container(
-  //   height: 40,
-  //   width: 19.4 * SizeConfig.widthMultiplier,
-  //   // margin: EdgeInsets.only(top: 5, right: 15, bottom: 5),
-  //   decoration: BoxDecoration(
-  //     gradient: LinearGradient(colors: [
-  //       Colors.red,
-  //       Colors.purple,
-  //     ]),
-  //     borderRadius: BorderRadius.circular(45),
-  //   ),
-  // );
-
-  //   child: Stack(
-  //     // overflow: Overflow.visible,
-  //     children: [
-  // Container(
-  //   margin: hasStory
-  //       ? EdgeInsets.all(0.83 * SizeConfig.widthMultiplier)
-  //       : null,
-  //   decoration: BoxDecoration(
-  //     borderRadius: BorderRadius.circular(45),
-  //     image: DecorationImage(image: NetworkImage(url), fit: BoxFit.cover),
-  //   ),
-  // ),
-  //       isOwner
-  //           ? Positioned(
-  //               bottom: 0,
-  //               right: 0.56 * SizeConfig.widthMultiplier,
-  //               child: addStoryIcon(),
-  //             )
-  //           : Container(),
-  //       Positioned(
-  //         bottom: 0,
-  //         child: Text(
-  //           username,
-  //           overflow: TextOverflow.ellipsis,
-  //         ),
-  //       ),
-  //     ],
-  //   ),
-  // );
 }
 
 addStoryIcon() {
